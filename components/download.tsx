11a5932352f251c02b6038ebdd8b05d2ebb2682f
Eduard Albrecht <eddysoulo91@gmail.com>
2022-08-13 14:34:08 +0200

    fix: itch io iframe

--- components/download.tsx
+++ components/download.tsx
@@ -6,8 +6,8 @@ import { Suspense } from "react";
 export default function Download(){
     return(
         <div id="download" className="w-full flex flex-col items-center gap-8 p-8 bg-gradient-to-tr from-indigo-600 to-violet-400" >
-            <h2 className="text-4xl font-bold text-indigo-100 text-center" >Buy QuizGenerator by ealbrecht.dev</h2>
-            <iframe src="https://itch.io/embed/1657976" width="552" height="167">
+            <h2 className="text-3xl font-bold text-indigo-100 text-center xl:text-4xl" >Buy QuizGenerator by ealbrecht.dev</h2>
+            <iframe src="https://itch.io/embed/1657976" className="max-w-full" width="552" height="167">
                 <Link href="https://edlix-studios.itch.io/quiz-generator" >
                     <a>Quiz Generator by ealbrecht.dev</a>
                 </Link>

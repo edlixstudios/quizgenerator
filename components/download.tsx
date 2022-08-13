488f479673651b8a1b4465786fdc08a0fecd6949
Eduard Albrecht <eddysoulo91@gmail.com>
2022-08-13 14:20:00 +0200

    fixed by text to center

--- components/download.tsx
+++ components/download.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export default function Download(){
     return(
         <div id="download" className="w-full flex flex-col items-center gap-8 p-8 bg-gradient-to-tr from-indigo-600 to-violet-400" >
-            <h2 className="text-4xl font-bold text-indigo-100" >Buy QuizGenerator by ealbrecht.dev</h2>
+            <h2 className="text-4xl font-bold text-indigo-100 text-center" >Buy QuizGenerator by ealbrecht.dev</h2>
             <iframe src="https://itch.io/embed/1657976" width="552" height="167">
                 <Link href="https://edlix-studios.itch.io/quiz-generator" >
                     <a>Quiz Generator by ealbrecht.dev</a>
